c645fd5c70b97e8f4af39ce630c9b3ec87b7f1d7
Matt MacNeil <matt@mattmacneil.com>
2017-04-20 17:15:07 -0400

    Added existing files.

---
+++ head.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+
+	<title>
+		KINGS of VAPES :: Lynn, MA :: Vaporizers and Vape Accessories
+	</title>
+  <link rel="shortcut icon" type="image/x-icon" href="images/favicon32.png">
+  <link rel="icon" href="images/favicon.png" type="image/x-icon">
+
+  <meta name="author" content="mattmacneil.com">
+  <meta name="description" content="Kings of Vapes is a Vaporizer and Vape Accessory Shop in Lynn, MA">
+  <meta name="keywords" content="vaporizer, vape, vaporizor store, vape store, vaporizer pen, vape shop, Lynn, Lynn MA, North Shore, Massachusetts">
+  <meta name="format-detection" content="telephone=no">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="css/styles/style.css" rel="stylesheet">
+
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-77339172-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
+
+</head>
+<body>
---
+++ index.php
@@ -0,0 +1,113 @@
+<?php
+    include "head.php";
+?>
+
+<div id="page-container">
+
+	<div id="page-container-overlay">
+
+		<div id="page-top">
+
+			<nav>
+
+				<div id="nav-inner">
+
+					<div id="nav-flag">
+						<div id="nav-flag-inner">
+							<em>follow:</em><br />
+
+							<div>
+							<a href="https://www.facebook.com/Kings-of-Vapes-1510478749244272/" class="noline" target="_blank"><i class="fa fa-facebook-official"></i></a>
+							<a href="https://www.instagram.com/kingsofvapes/" class="noline" target="_blank"><i class="fa fa-instagram"></i></a>
+							</div>
+
+							<div class="nav-flag-inner-snipe"></div><!-- nav flag inner snipe -->
+							<div class="nav-flag-inner-snipe-right"></div><!-- nav flag inner snipe -->
+						</div><!-- nav flag inner -->
+					</div><!-- nav flag -->
+
+					<div id="nav-title">
+						<b>&#9812;<br /></b>
+						<em>&bull;</em> Kings <span>of</span> Vapes <em>&bull;</em>
+					</div><!-- nav title -->
+
+					<div id="nav-phone">
+						<div id="nav-phone-inner">
+
+							<a href="tel:339-440-4169" class="noline">
+							<span>
+								<i class="fa fa-phone"></i> 339&dash;440&dash;4169
+							</span>
+							</a>
+
+						</div><!-- nav phone inner -->
+					</div><!-- nav phone -->
+
+					<div class="clearfix"></div>
+
+				</div><!-- nav inner -->
+
+			</nav><!-- nav -->
+
+			<main>
+
+				<div id="maparea">
+
+					<a href="https://www.google.com/maps/place/254+Western+Ave,+Lynn,+MA+01904/" class="noline" target="_blank">
+					<span>
+						<i class="fa fa-map-marker"></i> 254 — 256 Western Avenue, Lynn MA <em>[map]</em>
+					</span>
+					</a>
+
+				</div><!-- maparea -->
+
+				<div id="content">
+
+					<h1>We're Lynn's Vaporizer & Vape Supply Shop.</h1><br />
+
+					<span>
+						We're Open 
+						Monday Through Friday 10<b>am</b> &dash; 7<b>pm</b>, Saturday 11<b>am</b> &dash; 7<b>pm</b>.</span><br />
+
+					<a href="mailto:info@kingsofvapes781.com" class="noline">Have a Question? Email info@kingsofvapes781.com</a>
+
+
+				</div><!-- content -->
+
+			</main><!-- main content -->
+
+		</div><!-- page top -->
+
+		<div id="page-bottom">
+
+			<footer>
+
+				<div id="footer-inner">
+
+					<div>
+						<div class="copyright">
+							&copy; 2017 Kings of Vapes
+						</div>
+					</div>
+
+					<div>
+					<a href="mailto:info@kingsofvapes781.com" class="noline"><i class="fa fa-envelope"></i></a>
+					<a href="https://www.facebook.com/Kings-of-Vapes-1510478749244272/" target="_blank" class="noline"><i class="fa fa-facebook-official"></i></a>
+					<a href="https://www.instagram.com/kingsofvapes/" target="_blank" class="noline"><i class="fa fa-instagram"></i></a>
+					</div>
+
+				</div><!-- footer inner -->
+
+			</footer><!-- footer -->
+
+		</div><!-- page bottom -->
+
+	</div><!-- page container overlay -->
+
+</div><!-- page container -->
+
+<?php 
+
+    include "footer.php";
+
+?>
